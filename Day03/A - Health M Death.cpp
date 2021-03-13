d6596b4e1f53ff9eb524b7a17ced54a8028f48e9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-13 19:15:05 +0530

    Create A - Health M Death.cpp

---
+++ Day03/A - Health M Death.cpp
@@ -0,0 +1,17 @@
+/*
+Platform :- Atcoder
+Problem :- A - Health M Death
+Contest :- Panasonic Programming Contest (AtCoder Beginner Contest 195)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long m,x;
+    cin>>m>>x;
+    
+    if(x%m)cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
+    
+}
