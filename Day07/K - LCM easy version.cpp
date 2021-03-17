caea19efba153e11e126c59d7662b2106e3bdc35
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-17 22:25:48 +0530

    Update K - LCM easy version.cpp

--- Day07/K - LCM easy version.cpp
+++ Day07/K - LCM easy version.cpp
@@ -3,3 +3,40 @@ Platform :- Codeforces
 Problem :- K - LCM easy version
 Contest :- Codeforces Round 708 Div 2
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    for(int z=1;z<=t;++z){
+          long long n;
+          int k;
+          cin>>n>>k;
+          
+          if(n==k){
+              cout<<1<<" "<<1<<" "<<1<<endl;
+          }
+          else{
+              if(n%2){
+                  long long z=n/2;
+                  cout<<1<<" "<<z<<" "<<z<<endl;
+              }
+              else{
+                  long long z=(n)/2;
+                  
+                  
+                  if(z%2){
+                      cout<<2<<" "<<((n-1)/2)<<" "<<((n-1)/2)<<endl;
+                  }
+                  else{
+                   
+                      cout<<(z/2)<<" "<<(z/2)<<" "<<z<<endl;
+                  
+                  }
+                   
+              }
+          }
+    }
+}
