b9761c90622543382dc89d649fda35210be4b2b0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-17 22:16:30 +0530

    Update A - Meximization.cpp

--- Day07/A - Meximization.cpp
+++ Day07/A - Meximization.cpp
@@ -3,3 +3,39 @@ Platform :- Codeforces
 Problem :- A - Meximization
 Contest :- Codeforces Round 708 Div 2
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    for(int z=1;z<=t;++z){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        set<long long>P;
+        map<long long,int>Q;
+        for(int i=0;i<n;++i){
+           cin>>A[i];   
+           Q[A[i]]++;
+           P.insert(A[i]);
+        }
+        vector<long long>B;
+        for(auto x:P)B.push_back(x);
+        int m=B.size();
+        int i=0;
+         while(n>0){
+            if(Q[B[i]]){
+                cout<<B[i]<<" ";
+                Q[B[i]]--;
+                n--;
+            }    
+            
+            i++;
+            i%=m;
+         }
+         cout<<endl;
+    }
+}
