c603a2847831fe30277167e64b2f711d22069c82
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-19 23:17:51 +0530

    Update Keys and Rooms.cpp

--- Day09/Keys and Rooms.cpp
+++ Day09/Keys and Rooms.cpp
@@ -4,3 +4,38 @@ Problem :- Keys and Rooms
 Hint :- BFS + visisted array
 */
 
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int mx=0;
+        for(auto x:rooms){
+            for(auto y:x){
+                mx=max(mx,y);
+            }
+        }
+        vector<int> visited(mx+1,0);
+        queue<int>P;
+        P.push(0);
+        
+        while(!P.empty()){
+            int c=P.size();
+            for(int i=1;i<=c;++i){
+                int x=P.front();
+                
+                P.pop();
+                if(visited[x])continue;
+                for(int j=0;j<rooms[x].size();++j){
+                    //cout<<(rooms[x][j])<<" "<<endl;
+                    if(visited[rooms[x][j]])continue;
+                    P.push(rooms[x][j]);
+                }
+                visited[x]++;
+            }
+        }
+        
+        for(int i=0;i<=mx;++i){
+            if(visited[i]==0)return false;
+        }
+        return true;
+    }
+};
