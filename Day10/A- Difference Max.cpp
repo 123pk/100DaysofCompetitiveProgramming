976781463a38667c6d0bc360f0dfaa058c1c1365
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-20 22:21:03 +0530

    Create A- Difference Max.cpp

---
+++ Day10/A- Difference Max.cpp
@@ -0,0 +1,14 @@
+/*
+Platform :- Leetcode
+Problem :- A - Difference Max
+Contest :- Bi Weekly Contest 48
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    
+    cout<<(b-c)<<endl;
+}
