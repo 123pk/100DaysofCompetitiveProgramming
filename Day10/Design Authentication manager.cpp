f0f424627a52e63e470f3cc1894fc20bbd5f01e1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-20 22:11:31 +0530

    Update Design Authentication manager.cpp

--- Day10/Design Authentication manager.cpp
+++ Day10/Design Authentication manager.cpp
@@ -3,3 +3,52 @@ Platform :- Leetcode
 Contets :- Bi Weekly Contest 48
 Problem :- Design Authentication manager
 */
+
+class AuthenticationManager {
+public:
+    int d;
+    map<string,pair<int,int>>P;
+    set<string>Q;
+    AuthenticationManager(int timeToLive) {
+        d=timeToLive;
+    }
+    
+    void generate(string tokenId, int currentTime) {
+        if(Q.find(tokenId)!=Q.end()){
+            P[tokenId].first=currentTime;
+            P[tokenId].second=currentTime+d;
+        }
+        else{
+          P[tokenId]={currentTime,currentTime+d}; 
+         Q.insert(tokenId);
+        }
+    }
+    
+    void renew(string tokenId, int currentTime) {
+        if(Q.find(tokenId)!=Q.end()){
+            if((currentTime<P[tokenId].second) &&(currentTime>P[tokenId].first)){
+                P[tokenId]={currentTime,currentTime+d};
+            }
+        }
+    }
+    
+    int countUnexpiredTokens(int currentTime) {
+        int c=0;
+        for(auto x:P){
+            
+            if(x.second.second>currentTime){
+                //cout<<x.first<<" "<<x.second.first<<" "<<x.second.second<<endl;
+                c++;
+            }
+        }
+        return c;
+    }
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager* obj = new AuthenticationManager(timeToLive);
+ * obj->generate(tokenId,currentTime);
+ * obj->renew(tokenId,currentTime);
+ * int param_3 = obj->countUnexpiredTokens(currentTime);
+ */
