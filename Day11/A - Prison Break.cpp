756fa6d5b930d4609a4b95bd4260342edc1a2064
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-21 21:13:06 +0530

    Create A - Prison Break.cpp

---
+++ Day11/A - Prison Break.cpp
@@ -0,0 +1,21 @@
+/*
+Platform :- Codeforces
+Problem :- A - Prison Break
+Contest :- Codeforces Round 709
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,m;
+        cin>>n>>m;
+        
+        long long ans=n*m;
+        cout<<ans<<endl;
+    }
+}
