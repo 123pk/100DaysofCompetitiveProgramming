f6dd189b93a2dd0a550a20e2259c0d94c773128b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-24 23:39:52 +0530

    Update Reorder Power of 2.cpp

--- Day14/Reorder Power of 2.cpp
+++ Day14/Reorder Power of 2.cpp
@@ -3,3 +3,22 @@ Platform :- Leetcode
 Problem :- Reorder Power of 2
 Event :- March Daily challenge
 */
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        
+        string s=to_string(n);
+        int f=0;
+        sort(s.begin(),s.end());
+        do{
+            if(s[0]!='0'){
+           int m=stoi(s);
+           string z=bitset<64>(m).to_string();
+            if(count(z.begin(),z.end(),'1')==1){
+                f=1;break;
+            }
+            }
+        }while(next_permutation(s.begin(),s.end()));
+        return f;
+    }
+};
