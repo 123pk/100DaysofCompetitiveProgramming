401eac8e5ee099fcc93f1ba494a72008ceeb8d03
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-25 22:51:12 +0530

    Update C- Double ended string.cpp

--- Day15/C- Double ended string.cpp
+++ Day15/C- Double ended string.cpp
@@ -4,3 +4,50 @@ Problem :- C - Double Ended string
 Contest :- Coddeforces Round #703 Div 3
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+          string s1,s2;
+          cin>>s1>>s2;
+          
+          int ans=0;
+          int mx=0;
+           
+          for(int i=0;i<s1.size();++i){
+              
+              
+              for(int j=0;j<s2.size();++j){
+                 
+                  if(s1[i]==s2[j]){
+                       int c=0;
+                      for(int k=0;(k+j)<(s2.size()) && (k+i)<s1.size();++k){
+                          if(s1[k+i]==s2[k+j]){
+                              c++;
+                          }
+                          else break;
+                      }
+                      //cout<<c<<" ";
+                      if(mx<c){
+                      mx=c;
+                      ans=(s1.size()-c)+(s2.size()-c);
+                      }
+                  }
+              }
+               
+              
+          }
+          
+          if(mx==0){
+              ans=(s1.size()+s2.size());
+              cout<<ans<<endl;
+          }
+          else{
+              cout<<ans<<endl;
+          }
+    }
+}
