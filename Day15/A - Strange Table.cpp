85f2cb612b905ae05de4308eef8ccfbc0d98a1ae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-25 22:47:28 +0530

    Update A - Strange Table.cpp

--- Day15/A - Strange Table.cpp
+++ Day15/A - Strange Table.cpp
@@ -4,3 +4,27 @@ Problem :- A - Strange Table
 Contest :- Codeforces Round #710 Div 3
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n,m,x;
+        cin>>n>>m>>x;
+        
+        long long r=(x/n);
+        if(x%n)r++;
+        long long c=(x%n) ;
+        if(x%n==0){
+            c=n;
+        }
+        //cout<<c<<" "<<r<<endl;
+        c=(c-1)*m;
+        c+=r;
+  
+        cout<<c<<endl;
+    }
+}
