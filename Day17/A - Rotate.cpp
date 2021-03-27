a9f559810ce5b40de8b62103345f339998c8b434
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-27 22:19:25 +0530

    Update A - Rotate.cpp

--- Day17/A - Rotate.cpp
+++ Day17/A - Rotate.cpp
@@ -3,3 +3,13 @@ Platform :- Atcoder
 Problem :- A - Rotate
 Contest :- Atcoder Beginner Contest 197
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    for(int i=1;i<s.size();++i)cout<<s[i];
+    for(int i=0;i<1;++i)cout<<s[i];
+    cout<<endl;
+}
