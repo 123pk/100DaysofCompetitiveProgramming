879a7b6123230e412622afe5e83837f16bc5352f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-27 22:43:42 +0530

    Update Equal Elements.cpp

--- Day13/Equal Elements.cpp
+++ Day13/Equal Elements.cpp
@@ -5,3 +5,25 @@ Contest :- March Circuits
 
    SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n;
+		cin>>n;
+
+		long long A[n];
+		int c=0;
+		for(int i=0;i<n;++i){
+			cin>>A[i];
+			if(A[i]%2)c++;
+		}
+
+		int d=n-c;
+		cout<<(min(c,d))<<endl;
+	}
+}
