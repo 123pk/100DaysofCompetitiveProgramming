8944834a703a080f44bc162c3d0a9059c89baf4a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-28 22:34:42 +0530

    Update A - Odd vs Even.cpp

--- Day18/A - Odd vs Even.cpp
+++ Day18/A - Odd vs Even.cpp
@@ -3,3 +3,32 @@ Platform :- Atcoder
 problem :- A - Odd vs Even
 Contest :- Atcoder Regular contes 116
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        if(n%2){
+            cout<<"Odd"<<endl;
+        }
+        else{
+             if(n==2){
+                 cout<<"Same"<<endl;
+             }
+             else{
+                 n/=2;
+                 if(n%2){
+                     cout<<"Same"<<endl;
+                 }
+                 else cout<<"Even"<<endl;
+             }
+        }
+    }
+}
