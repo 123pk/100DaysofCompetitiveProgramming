eb361ed21bbf538f58cb060082a8fc746cef6f49
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-28 22:37:19 +0530

    Update Number of different Integers in string.cpp

--- Day18/Number of different Integers in string.cpp
+++ Day18/Number of different Integers in string.cpp
@@ -3,3 +3,64 @@ Platform :- Leetcode
 Contest :- Leetcode Weekly contest 234
 Problem :- Number of different Integer in string
 */
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+        set<string>P;
+        string temp;
+        for(auto x:word){
+            if('0'<=x && x<='9'){
+                  
+                    temp+=x;
+            }
+            else{
+                if(temp.size()){
+                    if(temp.size()==1){
+                        P.insert(temp);
+                    }
+                    else{
+                        string z;
+                     int f=0;
+                     for(int j=0;j<temp.size();++j){
+                         if(f){
+                             z+=temp[j];
+                         }
+                         else{
+                             if(temp[j]=='0')continue;
+                             else{
+                                 z+=temp[j];
+                                 f=1;
+                             }
+                         }
+                     }
+                     temp=z;
+                        P.insert(temp);
+                    }
+                    
+                    temp="";
+                }
+            }
+        }
+        
+        if(temp.size()==1)P.insert(temp);
+        else if(temp.size()>1){
+            string z;
+                     int f=0;
+                     for(int j=0;j<temp.size();++j){
+                         if(f){
+                             z+=temp[j];
+                         }
+                         else{
+                             if(temp[j]=='0')continue;
+                             else{
+                                 z+=temp[j];
+                                 f=1;
+                             }
+                         }
+                     }
+                     temp=z;
+                        P.insert(temp);
+        }
+        return P.size();
+    }
+};
