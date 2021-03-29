3475038d0cd38cd8aa21617ccc857da145c991ce
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-29 22:10:13 +0530

    Update A - GCD.cpp

--- Day19/A - GCD.cpp
+++ Day19/A - GCD.cpp
@@ -4,3 +4,30 @@ Problem :- A - GCD
 Contest :- Codeforces Round 711 Div 2
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        while(1){
+            
+            long long temp=n;
+        long long tot=0;
+        while(temp){
+            tot+=temp%10;
+            temp/=10;
+        }
+        
+        if(__gcd(n,tot)>1)break;
+        n++;
+        }
+        
+        cout<<n<<endl;
+    }
+}
