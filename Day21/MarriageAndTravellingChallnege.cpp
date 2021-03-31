ab0a9d37c257c0839c50fbea6f59609428207f84
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-31 23:04:35 +0530

    Update MarriageAndTravellingChallnege.cpp

--- Day21/MarriageAndTravellingChallnege.cpp
+++ Day21/MarriageAndTravellingChallnege.cpp
@@ -4,3 +4,20 @@ Problem :- MarriageAndTravellingChallenege
 Contest :- SRM 803
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class  MarriageAndTravelingChallenge{
+
+public:
+   string solve(string S){
+       string ans;
+       int d=1;
+       for(int i=0;i<S.size();i+=d){
+           ans+=S[i];
+           d++;
+       }
+       
+       return ans;
+   }
+};
