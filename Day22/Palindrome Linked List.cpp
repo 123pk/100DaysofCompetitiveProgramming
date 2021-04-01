a6b9a465e96369762aa2fac9e88ef2da5a86802c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-01 22:45:28 +0530

    Update Palindrome Linked List.cpp

--- Day22/Palindrome Linked List.cpp
+++ Day22/Palindrome Linked List.cpp
@@ -2,3 +2,35 @@
 Platform :- Leetcode
 Problem :- Palindrome Linked List
 */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL)
+        {
+            return true;
+        }
+        vector<int>P;
+        ListNode*p=NULL;
+        p=head;
+        
+        if(p->next==NULL)
+        {
+            return true;
+        }
+        
+        while(p)
+        {
+            P.push_back(p->val);
+            p=p->next;
+        }
+        
+        for(int i=0;i<P.size()/2;++i)
+        {
+            if(P[i]!=P[P.size()-1-i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
