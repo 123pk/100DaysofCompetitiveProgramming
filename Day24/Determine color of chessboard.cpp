1cdab8569e93272f6a1871942774c5699caa1919
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-03 22:26:44 +0530

    Update Determine color of chessboard.cpp

--- Day24/Determine color of chessboard.cpp
+++ Day24/Determine color of chessboard.cpp
@@ -3,3 +3,15 @@ Platform :- Leetcode
 Problem :- Determine Color of Chessboard
 Contest :- Biweekly contest 49
 */
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        map<char,int>P;
+        for(char ch='a';ch<='h';++ch){
+            P[ch]=(ch-'a')+1;
+        }
+        
+        int temp=P[coordinates[0]]+coordinates[1]-'0';
+        return temp%2;
+    }
+};
