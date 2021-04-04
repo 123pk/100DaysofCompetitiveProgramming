d65354368ac996cbba5467b90e3979b83f4b7826
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-04 10:07:25 +0530

    Update Finding the user active time.cpp

--- Day25/Finding the user active time.cpp
+++ Day25/Finding the user active time.cpp
@@ -3,3 +3,23 @@ Platform :- Leetcode
 Problem :- Finding the user actve time
 Contest :- Weekly contest 235
 */
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        map<int,int>Q;
+        map<int,set<int>>P;
+        for(auto x:logs){
+            P[x[0]].insert(x[1]);
+        }
+        
+        vector<int>ans(k);
+        for(auto x:P){
+            Q[x.second.size()-1]++;
+        }
+        
+        for(int i=0;i<k;++i){
+            ans[i]=Q[i];
+        }
+        return ans;
+    }
+};
