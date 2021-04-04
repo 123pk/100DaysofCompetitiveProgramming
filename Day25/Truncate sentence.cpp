720d25d3c307be5487c3e6dd02f7fbfdb2c93b32
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-04 10:04:53 +0530

    Update Truncate sentence.cpp

--- Day25/Truncate sentence.cpp
+++ Day25/Truncate sentence.cpp
@@ -3,3 +3,29 @@ Platform :- Leetcode
 Problem :- Truncate sentence
 Contest :- Weekly contest 235
 */
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans;
+        s+=' ';
+        vector<string>P;
+        string temp;
+        for(int i=0;i<s.size();++i){
+            if(s[i]==' '){
+                if(k){
+                ans+=temp;
+                    if(k>1){
+                       ans+=' '; 
+                    }
+                k--;
+                temp="";
+                }
+                else break;
+            }
+            else{
+                temp+=s[i];
+            }
+        }
+        return ans;
+    }
+};
