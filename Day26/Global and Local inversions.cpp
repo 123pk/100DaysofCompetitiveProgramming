3e7ca3ca4a9781b9c7a13e576a99c6040abb4922
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-05 22:44:18 +0530

    Update Global and Local inversions.cpp

--- Day26/Global and Local inversions.cpp
+++ Day26/Global and Local inversions.cpp
@@ -3,3 +3,14 @@ Platform :- Leetcode
 Problem :- Global and Local inversion
 Event :- April daily challenge
 */
+class Solution {
+public:
+   bool isIdealPermutation(vector<int>& A) {
+        int cmax = 0, n = A.size();
+        for (int i = 0; i < n - 2; ++i) {
+            cmax = max(cmax, A[i]);
+            if (cmax > A[i + 2]) return false;
+        }
+        return true;
+    }
+};
