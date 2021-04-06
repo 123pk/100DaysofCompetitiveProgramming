9bfcdbf547b2b277ca88823f656f9235c373fb78
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-06 23:22:33 +0530

    Create Lucky Boundaries.cpp

---
+++ Day27/Lucky Boundaries.cpp
@@ -0,0 +1,19 @@
+/*
+Platform :- Codechef
+Problem :- Lucky Boundaries
+Contest :- International coding marathon 2
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        cout<<n<<endl;
+    }
+}
