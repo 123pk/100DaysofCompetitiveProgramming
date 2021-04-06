16a5c0e3cc8b4a03349e9d9ec959c359c75079b6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-06 23:20:04 +0530

    Update Minimum Operations to Make Array Equal.cpp

--- Day27/Minimum Operations to Make Array Equal.cpp
+++ Day27/Minimum Operations to Make Array Equal.cpp
@@ -3,3 +3,35 @@ Platform :- Leetcode
 Problem :- Minimum Operations to Make Array Equal
 Event :- Leetcode Daily challenge
 */
+class Solution {
+public:
+    int minOperations(int n) {
+        if(n%2)
+        {
+            int x1=n/2;
+            int val=(2*x1)+1;
+            int c=0;
+            for(int i=0;i<x1;++i)
+            {   
+                int temp=(2*i)+1;
+                c+=abs(temp-val);
+            }
+            return c;
+            
+        }
+        else
+        {
+            int x1=n/2;
+            
+            int val=(2*x1);
+            int c=0;
+            for(int i=0;i<x1;++i)
+            {   
+                int temp=(2*i)+1;
+                c+=abs(temp-val);
+            }
+            return c;
+            
+        }
+    }
+};
