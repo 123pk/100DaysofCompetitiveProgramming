e46e5dd9f9686e1b5aa57276f1f37c7738a0be5f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-10 23:37:34 +0530

    Update A - Spy Detected.cpp

--- Day31/A - Spy Detected.cpp
+++ Day31/A - Spy Detected.cpp
@@ -3,3 +3,44 @@ Platform :- Codeforces
 Contest:- Codeforces Round 713 Div 3
 Problem :- A - Spy Detected !
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        map<long long,int>P;
+        long long A[n];
+        for(int i=0;i<n;++i){
+            
+            cin>>A[i];
+            P[A[i]]++;
+        }
+        
+        long long temp=0;
+        if(A[0]==A[1]){
+            temp=A[0];
+        }
+        else if(A[1]==A[2]){
+            temp=A[1];
+        }
+        else if(A[0]==A[2]){
+            temp=A[0];
+        }
+        
+        int index=0;
+        for(int i=0;i<n;++i){
+            if(temp!=A[i]){
+                index=i+1;
+                break;
+            }
+        }
+        
+        cout<<(index)<<endl;
+    }
+}
