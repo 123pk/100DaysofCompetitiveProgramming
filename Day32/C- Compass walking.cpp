61497a5dd1843fa2f74a06947b3e680bc6b8de75
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-11 22:01:27 +0530

    Update C- Compass walking.cpp

--- Day32/C- Compass walking.cpp
+++ Day32/C- Compass walking.cpp
@@ -3,3 +3,31 @@ Platform :- Atcoder
 Problem :- C - Compass walking
 Contest :- Atcoder Beginner Contest 198
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+float pk(int x1, int y1, int x2, int y2)
+{
+    // Calculating distance
+    return sqrt(pow(x2 - x1, 2) +
+                pow(y2 - y1, 2) * 1.0);
+}
+
+int main(){
+     long long r,x,y;
+     cin>>r>>x>>y;
+     
+    float tot=pk(x,y,0,0);
+    
+    if((tot==r)){
+       cout<<1<<endl;
+      exit(0);
+    }
+    if((tot<=(2*r))){
+      cout<<2<<endl;
+      exit(0);
+    }
+    float z=tot/r;
+    long long ans=ceil(z);
+     cout<<ans<<endl;
+}
