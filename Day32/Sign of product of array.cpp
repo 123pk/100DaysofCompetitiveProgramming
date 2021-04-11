80ea2090b7b076c1c929b5227dcef71262e4b54c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-11 21:34:11 +0530

    Update Sign of product of array.cpp

--- Day32/Sign of product of array.cpp
+++ Day32/Sign of product of array.cpp
@@ -3,3 +3,21 @@ Platform :- Leetcode
 Problem :- Sign of product of array
 Contest :- Weekly contest 236
 */
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int f=0,c=0;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]==0){
+                f=1;break;
+            }
+            else{
+                if(nums[i]<0)c++;
+            }
+        }
+        
+        if(f)return 0;
+        if(c%2)return -1;
+        return 1;
+    }
+};
