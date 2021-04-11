8ec3ba33b7e63f618ac7b2ab5a44e3960ce5b4fb
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-11 21:40:36 +0530

    Update A - Div.cpp

--- Day32/A - Div.cpp
+++ Day32/A - Div.cpp
@@ -3,3 +3,12 @@ Platfrom :- Atcoder
 Problem :- A - Div
 Contest :- Atcoder Beginner Contest 198
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    cout<<(n-1)<<endl;
+}
