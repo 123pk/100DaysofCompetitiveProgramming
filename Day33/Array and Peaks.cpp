d6b0ac170a0059f1b9df2ecb5f0757e59e41d6fd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-12 22:45:33 +0530

    Update Array and Peaks.cpp

--- Day33/Array and Peaks.cpp
+++ Day33/Array and Peaks.cpp
@@ -3,3 +3,51 @@ Platform :- Codeforces
 Contest :- Codeforces Round #714 Div2
 Problem :- Array and Peaks
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        
+        int temp=(n-1)/2;
+        
+        if(temp<k){
+            cout<<"-1"<<endl;
+        }
+        else{
+            vector<int>P;
+            for(int i=1;i<=(n+1)/2;++i){
+                P.push_back(i);
+            }
+            
+            vector<int>Q;
+            for(int i=((n+1)/2 )+1;i<=n;++i){
+                Q.push_back(i);
+            }
+            
+            int i=0,j=0;
+            while(k--){
+                cout<<P[i]<<" ";
+                if(j==Q.size())break;
+                cout<<Q[j]<<" ";
+                i++;
+                j++;
+            }
+            
+              while(i<P.size()){
+                  cout<<P[i]<<" ";
+                  i++;
+              }
+                while(j<Q.size()){
+                    cout<<Q[j]<<" ";
+                        j++;
+                }
+            
+            cout<<endl;
+        }
+    }
+}
