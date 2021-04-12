1a44cb504f09d9b2e4596fb709791cd19c8ee229
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-12 22:46:40 +0530

    Update Deepest Leaves Sum.cpp

--- Day33/Deepest Leaves Sum.cpp
+++ Day33/Deepest Leaves Sum.cpp
@@ -2,3 +2,30 @@
 Platform :- Leetcode
 Problem :- Deepest Leaves Sum
 */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        
+        int ans=0;
+        if(!root)return 0;
+       queue<TreeNode*>P;
+        P.push(root);
+       while(!P.empty()){
+           int c=P.size();
+           int tot=0;
+           for(int i=1;i<=c;++i){
+               TreeNode*temp=P.front();
+               P.pop();
+               tot+=temp->val;
+               if(temp->left){
+                   P.push(temp->left);
+               }
+               if(temp->right){
+                   P.push(temp->right);
+               }
+           }
+           ans=tot;
+       }
+        return ans;
+    }
+};
