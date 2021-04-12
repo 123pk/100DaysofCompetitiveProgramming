573dc60f64e5509d20aee12cbab81bdfec4ad152
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-12 22:49:57 +0530

    Update A - Review Site.cpp

--- Day33/A - Review Site.cpp
+++ Day33/A - Review Site.cpp
@@ -3,3 +3,23 @@ Platform :- Codeforces
 Contest :- Educational Round 107 
 Problem :- A - Review Site
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int ans=0;
+        long long A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(A[i]!=2)ans++;
+        }
+        cout<<ans<<endl;
+    }
+}
