3ad33bb57bcc08d5da4c3413b471c3eb5becffa7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-14 22:45:24 +0530

    Update Partition List.cpp

--- Day35/Partition List.cpp
+++ Day35/Partition List.cpp
@@ -3,3 +3,64 @@ Platform :- Leetcode
 Problem :- Partition List
 Event :-  April Daily challenge
 */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        vector<int>P,Q;
+        ListNode*temp=head;
+        
+        while(temp){
+            if(temp->val<x){
+                P.push_back(temp->val);
+            }
+            else Q.push_back(temp->val);
+            temp=temp->next;
+        }
+        
+        ListNode*ans=NULL;
+        temp=NULL;
+        //sort(P.begin(),P.end());
+        for(auto x:P){
+           
+            if(!ans){
+            ListNode* y=new ListNode(x);
+            temp=y;
+            temp->next=NULL;
+            ans=temp;
+           }
+            else{
+                ListNode* y=new ListNode(x);
+                temp->next=y;
+                y->next=NULL;
+                temp=temp->next;
+            }
+        }
+        
+        for(auto x:Q){
+            if(!ans){
+            ListNode* y=new ListNode(x);
+            temp=y;
+            temp->next=NULL;
+            ans=temp;
+           }
+            else{
+                ListNode* y=new ListNode(x);
+                temp->next=y;
+                y->next=NULL;
+                temp=temp->next;
+            }
+        }
+        
+        return ans;
+    }
+};
