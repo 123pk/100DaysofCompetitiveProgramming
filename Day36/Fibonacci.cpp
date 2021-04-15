eab92c3919a062e02b3ed3e2f3596ad992feb210
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-15 23:06:13 +0530

    Update Fibonacci.cpp

--- Day36/Fibonacci.cpp
+++ Day36/Fibonacci.cpp
@@ -3,3 +3,16 @@ Platform :- Leetcode
 Problem :- Fibonacci
 Event :- April Daily challenge
 */
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)return n;
+        int s=0,t=1,ans=0;
+        for(int i=2;i<=n;++i){
+            ans=s+t;
+            s=t;
+            t=ans;
+        }
+        return ans;
+    }
+};
