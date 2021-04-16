d700a60836665a20cbfe21e83756d62552260b17
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-16 22:38:03 +0530

    Update GOTHAM.cpp

--- Day37/GOTHAM.cpp
+++ Day37/GOTHAM.cpp
@@ -3,3 +3,43 @@ Platform :- Codechef
 Contest :- SPYBITS IIT BHU
 Problem :- GOTHAM
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    long long A[n];
+    set<int>P;
+    for(int i=0;i<n;++i){
+        cin>>A[i];
+        P.insert(i);
+    }
+    
+    int q;
+    cin>>q;
+    
+    while(q--){
+        int x;
+        long long k;
+        cin>>x>>k;
+        
+        x-=1;
+        auto it =P.lower_bound(x);
+        long long ans=0;
+        
+        while(it!=P.end()  &&k>0){
+            int z=*it;
+            int d=z-x;
+            long long temp=min(A[z],k);
+            A[z]-=temp;
+            k-=temp;
+            ans+=(temp*d);
+            if(A[z]==0)P.erase(it);
+            it=P.lower_bound(x);
+        }
+        
+        cout<<ans<<endl;
+    }
+}
