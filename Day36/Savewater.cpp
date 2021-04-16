c530ef6a8a8abb6cde7daccacfe55d5355cee4cd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-16 22:35:56 +0530

    Update Savewater.cpp

--- Day36/Savewater.cpp
+++ Day36/Savewater.cpp
@@ -3,3 +3,21 @@ Platform :- Codechef
 Contest :- SPYBITS IIT BHU
 Problem :- Savewater
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long H,x,y,c;
+        cin>>H>>x>>y>>c;
+        
+        long long temp=x+(y/2);
+        temp*=H;
+        
+        if((temp<=c))cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
