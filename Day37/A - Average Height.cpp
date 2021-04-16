92231fda4ab594cac6e123656c2bf0e53a05e81b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-16 22:40:46 +0530

    Update A - Average Height.cpp

--- Day37/A - Average Height.cpp
+++ Day37/A - Average Height.cpp
@@ -3,3 +3,29 @@ Platform :- Codeforces
 Contest :- Codeforces Round 715 Div 2
 Problem :- A - Average Height
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long long A[n];
+        vector<long long>B,C;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(A[i]%2){
+                B.push_back(A[i]);
+            }
+            else C.push_back(A[i]);
+        }
+        
+        for(auto x:B)cout<<x<<" ";
+        for(auto x:C)cout<<x<<" ";
+        cout<<endl;
+    }
+}
