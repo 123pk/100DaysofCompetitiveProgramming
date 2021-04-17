e6006ec9fc7e57f5a5179889cbfd14d2a3028fc8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-17 22:16:59 +0530

    Update Minimum operation to make array increasing.cpp

--- Day38/Minimum operation to make array increasing.cpp
+++ Day38/Minimum operation to make array increasing.cpp
@@ -3,3 +3,17 @@ Platform :- Leetcode
 Problem :- Minimum Operation to make the array increasing
 Contest :- Biweekly contest 50
 */
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans=0;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]>nums[i-1])continue;
+            int temp=nums[i-1]-nums[i];
+            temp++;
+            ans+=temp;
+            nums[i]+=temp;
+        }
+        return ans;
+    }
+};
