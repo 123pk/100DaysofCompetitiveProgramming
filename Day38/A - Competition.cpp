ec7a9794cba60bedf347503fd8d86efdd65c56ed
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-17 22:23:59 +0530

    Update A - Competition.cpp

--- Day38/A - Competition.cpp
+++ Day38/A - Competition.cpp
@@ -3,3 +3,15 @@ Platform :- Atcoder
 Contest :- Japanese Student Championship
 Problem :- A - Competition
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     double x,y,z;
+     cin>>x>>y>>z;
+     
+     double temp=y/x;
+     long long c=ceil(temp*z)-1;
+     
+     cout<<c<<endl;
+}
