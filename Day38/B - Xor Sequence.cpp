928037e4ea9dd507bd7a301f0aa7c4a2d343c439
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-17 22:23:39 +0530

    Update B - Xor Sequence.cpp

--- Day38/B - Xor Sequence.cpp
+++ Day38/B - Xor Sequence.cpp
@@ -3,3 +3,28 @@ Platform :- Atcoder
 Contest :- Japanese Student Championship
 Problem :- B - Xor Sequence
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n,m;
+     cin>>n>>m;
+     
+     long long A[n];
+     map<long long,int>P;
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+         P[A[i]]++;
+     }
+     
+     long long B[m];
+     for(int i=0;i<m;++i){
+         cin>>B[i];
+         P[B[i]]++;
+     }
+     
+     for(auto x:P){
+         if(x.second==1)cout<<x.first<<" ";
+     }
+     cout<<endl;
+}
