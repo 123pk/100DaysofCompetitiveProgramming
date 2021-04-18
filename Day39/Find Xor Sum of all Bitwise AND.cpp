32a5046cc66b6ef3537cc9f480fc1583782c196f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-18 22:16:03 +0530

    Update Find Xor Sum of all Bitwise AND.cpp

--- Day39/Find Xor Sum of all Bitwise AND.cpp
+++ Day39/Find Xor Sum of all Bitwise AND.cpp
@@ -3,3 +3,32 @@ Platform :- Leetcode
 Contest :- Weekly contest 237
 Problem :- Find Xor sum of all bitwise AND
 */
+class Solution {
+public:
+    int getXORSum(vector<int>& arr1, vector<int>& arr2) {
+        vector<int>P(32,0);
+        for(int i=0;i<arr2.size();++i){
+            string temp=bitset<32>(arr2[i]).to_string();
+            reverse(temp.begin(),temp.end());
+            for(int j=0;j<32;++j){
+                if(temp[j]=='1')P[j]++;
+            }
+        }
+        
+        int ans=0;
+        for(int i=0;i<arr1.size();++i){
+            string temp=bitset<32>(arr1[i]).to_string();
+            reverse(temp.begin(),temp.end());
+            for(int j=0;j<32;++j){
+                if(temp[j]=='1'){
+                    if(P[j]){
+                        int temp=pow(2,j);
+                       if(P[j]%2)ans=ans xor temp;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
