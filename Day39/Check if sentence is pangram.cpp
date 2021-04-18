bacfa40d4669ecc203c1fb0b1ea4d237d2af9d8b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-18 22:13:10 +0530

    Update Check if sentence is pangram.cpp

--- Day39/Check if sentence is pangram.cpp
+++ Day39/Check if sentence is pangram.cpp
@@ -3,3 +3,16 @@ Platform :- Leetcode
 Problem :- Check if sentence is Pangram
 Contest :- Weekly contest 237
 */
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        map<char,int>P;
+        if((sentence.size()<26))return false;
+        
+        for(auto x:sentence)P[x]++;
+        for(char ch='a';ch<='z';++ch){
+            if(P[ch]==0)return false;
+        }
+        return true;
+    }
+};
