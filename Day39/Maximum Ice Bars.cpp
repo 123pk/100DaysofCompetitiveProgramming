8b31a8ec7065af126485cd693f75761a6e8581d6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-18 22:15:44 +0530

    Update Maximum Ice Bars.cpp

--- Day39/Maximum Ice Bars.cpp
+++ Day39/Maximum Ice Bars.cpp
@@ -3,3 +3,20 @@ Platform :- Leetcode
 Contest :- Weekly contest 237
 Problem :- Maximum Ice Bars
 */
+class Solution {
+public:
+    int maxIceCream(vector<int>& cost, int coins) {
+        sort(cost.begin(),cost.end());
+        
+        int ans=0;
+        for(int i=0;i<cost.size();++i){
+            if(cost[i]<=coins){
+                coins-=cost[i];
+                ans++;
+            }
+            else break;
+        }
+        return ans;
+        
+    }
+};
