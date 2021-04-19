2f3e78f7bd78bf810e5e7c331d4ff9e3d2c487ab
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-19 15:21:47 +0530

    Update Increasing substring.cpp

--- Day40/Increasing substring.cpp
+++ Day40/Increasing substring.cpp
@@ -2,4 +2,54 @@
 Platform :- Coding competition with google
 Contest :- Google Kickstart Round B 2021
 Problem :- Increasing Substring
+Hint:- we will maintain a variable to count consecutive elements in increasing order till current element.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    for(int z=1;z<=t;++z){
+        cout<<"Case #"<<z<<": ";
+        
+        int n;
+        cin>>n;
+        
+        string s;
+        cin>>s;
+        
+        int c=0;
+        for(int i=0;i<s.size();++i){
+            if(i==0){
+                c++;
+                cout<<c<<" ";
+            }
+            else{
+                if(i==s.size()-1){
+                    if(s[i]>s[i-1]){
+                        c++;
+                        cout<<c<<" ";
+                    }
+                    else{
+                        c=1;
+                        cout<<c<<" ";
+                    }
+                }
+                else{
+                    if(s[i]>s[i-1]){
+                        c++;
+                        cout<<c<<" ";
+                    }
+                    else{
+                        c=1;
+                        cout<<c<<" ";
+                    }
+                }
+            }
+        }
+        cout<<endl;
+        
+    }
+}
