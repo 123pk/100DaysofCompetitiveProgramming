3b72df6de1aecb79b2a80094b5f4788aa78bda49
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-20 20:51:56 +0530

    Update Chocolate Monger.cpp

--- Day41/Chocolate Monger.cpp
+++ Day41/Chocolate Monger.cpp
@@ -3,3 +3,29 @@ Platform :- Codechef
 Contest :- Codechef April cook off 2021 Div 2
 Problem :- Chocolate Monger
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        
+        long long A[n];
+        set<long long>P;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            P.insert(A[i]);
+        }
+        
+        int k=n-x;
+        int temp=P.size();
+        if(temp<k){
+            cout<<temp<<endl;
+        }
+        else cout<<k<<endl;
+    }
+}
