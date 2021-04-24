16423d7e408ed99ca22b70798b16792096b3fc13
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-24 22:13:36 +0530

    Update B - Intersection.cpp

--- Day45/B - Intersection.cpp
+++ Day45/B - Intersection.cpp
@@ -3,3 +3,30 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner contest 199
 Problem :- B - Intersection
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin>>n;
+     
+     int mn=INT_MAX,mx=INT_MIN;
+     int A[n],B[n];
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+         mx=max(mx,A[i]);
+     }
+     
+     for(int i=0;i<n;++i){
+         cin>>B[i];
+         mn=min(mn,B[i]);
+     }
+     
+     //cout<<mx<<" "<<mn<<endl;
+     if(mn<mx){
+         cout<<0<<endl;
+         exit(0);
+     }
+     int ans=(mn-mx)+1;
+     cout<<ans<<endl;
+}
