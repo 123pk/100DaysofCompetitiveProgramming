c5ee920c13fe27784da99c2c6dd927efe433186e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-24 22:10:12 +0530

    Update A - Square Inequality.cpp

--- Day45/A - Square Inequality.cpp
+++ Day45/A - Square Inequality.cpp
@@ -3,3 +3,17 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner contest 199
 Problem :- A - Square Inequality
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long a,b,c;
+    cin>>a>>b>>c;
+    
+    a*=a;
+    b*=b;
+    c*=c;
+    long long temp=a+b;
+    if(temp<c)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
