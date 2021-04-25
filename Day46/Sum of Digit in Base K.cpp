7cbb294bf624f6f425ab6b5bbbe58bc60c5f1eed
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-25 23:09:24 +0530

    Update Sum of Digit in Base K.cpp

--- Day46/Sum of Digit in Base K.cpp
+++ Day46/Sum of Digit in Base K.cpp
@@ -3,3 +3,16 @@ Platform :- Leetcode
 Contest :- Weekly contest 238
 Problem :- Sum of Digit in Base K
 */
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int ans=0;
+        
+        while(n){
+            ans+=(n%k);
+            n=n/k;
+        }
+        
+        return ans;
+    }
+};
