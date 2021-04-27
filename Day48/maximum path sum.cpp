4268952585c506067dea8ae4729df7de53785683
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-27 23:05:34 +0530

    Update and rename maximum path sum.cpp to minium path sum.cpp

--- Day48/maximum path sum.cpp
+++
@@ -1,4 +0,0 @@
-/*
-Platform :- Leetcode
-Problem :- 64. minimum path sum
-*/
---
+++ Day48/minium path sum.cpp
@@ -0,0 +1,33 @@
+/*
+Platform :- Leetcode
+Problem :- 64. minimum path sum
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(i==0 || j==0){
+                    if(i==0 && j==0){
+                        dp[i][j]=grid[i][j];
+                    }
+                    else if(i==0){
+                        dp[i][j]=dp[i][j-1]+grid[i][j];
+                    }
+                    else dp[i][j]=dp[i-1][j]+grid[i][j];
+                }
+                else{
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+    
+};
