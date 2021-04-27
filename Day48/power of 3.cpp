57fb9ea860976392715e7986f3d55d3a5f3102bc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-27 23:09:39 +0530

    Update power of 3.cpp

--- Day48/power of 3.cpp
+++ Day48/power of 3.cpp
@@ -3,3 +3,19 @@ Platform :- Leetcode
 Problem :- Power of 3
 Event :- April Daily challenge
 */
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        while(n>=3){
+            if(n%3){
+               return false;
+            }
+            n/=3;
+            if(n==1)return true;
+        }
+        
+        if(n==1)return true;
+        return false;
+    }
+};
