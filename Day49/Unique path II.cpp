7bb043bb727f768f8c51c736b355ad05afb4ff28
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-28 20:49:42 +0530

    Update Unique path II.cpp

--- Day49/Unique path II.cpp
+++ Day49/Unique path II.cpp
@@ -3,3 +3,32 @@ Platform :- Leetcode
 Problem :- Unique Path II
 Event :- Leetcode April Daily challenge
 */
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& A) {
+        
+        int n=A.size();
+        int m=A[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<A.size();++i){
+            for(int j=0;j<A[i].size();++j){
+                if(i==0||j==0){
+                    if(i==0 && j==0){
+                        if(A[i][j]==0)dp[i][j]=1;
+                    }
+                    else if(i==0){
+                        if(A[i][j]==0)dp[i][j]=dp[i][j-1];
+                    }
+                    else{
+                        if(A[i][j]==0)dp[i][j]=dp[i-1][j];
+                    }
+                }
+                else{
+                    if(A[i][j]==0)dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
