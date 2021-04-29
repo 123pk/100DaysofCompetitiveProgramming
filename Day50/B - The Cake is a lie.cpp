181602c11cf9d287b0b5f062329d393ec00183fc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-29 22:59:58 +0530

    Update B - The Cake is a lie.cpp

--- Day50/B - The Cake is a lie.cpp
+++ Day50/B - The Cake is a lie.cpp
@@ -3,3 +3,20 @@ Platform :- Codeforces
 Contest :- Educational Round 108
 Problem :- The Cake is a lie
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        
+        int temp=n*m;
+        temp-=1;
+        if(temp==k)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
