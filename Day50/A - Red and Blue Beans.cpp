eb86861cc3873c7acdb8655a6a5afa05fbd5522b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-29 23:01:38 +0530

    Update A - Red and Blue Beans.cpp

--- Day50/A - Red and Blue Beans.cpp
+++ Day50/A - Red and Blue Beans.cpp
@@ -3,3 +3,21 @@ Platform :- Codeforces
 Contest :- Educational Round 108
 Problem :- A - Red and Blue Beans
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b,d;
+        cin>>a>>b>>d;
+        
+        long long  temp=min(a,b);
+        long long temp2=max(a,b);
+        
+        long long ans=(1+d)*temp;
+        if(ans<temp2)cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+}
