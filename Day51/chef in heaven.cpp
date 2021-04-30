6dc13a4e7d64046c0ea8da62a488dd2ac26d59fa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-04-30 22:58:13 +0530

    Update chef in heaven.cpp

--- Day51/chef in heaven.cpp
+++ Day51/chef in heaven.cpp
@@ -3,3 +3,30 @@ Platform :- Codechef
 Contest :- April Lunchtime 
 Problem :- chef in heaven (CCHEAVEN)
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int l;
+        cin>>l;
+        
+        string s;
+        cin>>s;
+        
+        int c=0,f=0;
+        for(int i=0;i<l;++i){
+            if(s[i]=='1')c++;
+            else c--;
+            
+            if(c>=0){f=1;break;}
+        }
+        
+        if(f)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
