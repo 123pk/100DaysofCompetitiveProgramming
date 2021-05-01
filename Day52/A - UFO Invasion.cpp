f34ee7a83c070f0fdf755bc3d1257d4d6bb6f7b8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-01 19:17:07 +0530

    Update A - UFO Invasion.cpp

--- Day52/A - UFO Invasion.cpp
+++ Day52/A - UFO Invasion.cpp
@@ -3,3 +3,26 @@ Platform :- Atcoder
 Problem :- A - UFO Invasion
 Contest :- Zone energy Programming contest
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    
+    string temp="ZONe";
+    int d=0;
+    for(int i=0;i<s.size();++i){
+        if(s[i]==temp[0]){
+            int f=0;
+            for(int j=0;j<temp.size();++j){
+                if(s[i+j]!=temp[j]){
+                    f=1;break;
+                }
+            }
+            if(f==0)d++;
+        }
+    }
+    cout<<d<<endl;
+}
