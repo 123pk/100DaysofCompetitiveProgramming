e35a0faa58ec71d36296b8babdd1a984e1698a11
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-02 23:27:51 +0530

    Update minimum Distance to the Target Element.cpp

--- Day53/minimum Distance to the Target Element.cpp
+++ Day53/minimum Distance to the Target Element.cpp
@@ -3,3 +3,17 @@ Platform :- Leetcode
 Problem :- Minimum Distance to the Target Element 
 Contest :- Weekly contest 239
 */
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]==target){
+                int temp=abs(start-i);
+                ans=min(ans,temp);
+            }
+        }
+        return ans;
+    }
+};
