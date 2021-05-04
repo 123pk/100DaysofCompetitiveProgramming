d2fba0cfd02f155fc0b3468ea48b05aa5742de9c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-04 20:58:23 +0530

    Update Non Decreasing array.cpp

--- Day55/Non Decreasing array.cpp
+++ Day55/Non Decreasing array.cpp
@@ -2,3 +2,44 @@
 Platform :- Leetcode
 Problem :- Non Decreasing Array
 */
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int f=0;
+         
+        int c=0;
+        
+        vector<int>z=nums;
+        for(int i=1;i<z.size();++i){
+            if(z[i]<z[i-1]){
+                z[i]=z[i-1];
+                c=i;
+                break;
+            }
+        }
+        
+      //  cout<<z[0]<<" ";
+        for(int i=1;i<z.size();++i){
+           // cout<<z[i]<<" ";
+            if(z[i]>=z[i-1])continue;
+            f=1;break;
+        }
+    //    cout<<endl;
+        
+        if(f==0)return true;
+        
+        vector<int>temp=nums;
+        temp[c-1]=temp[c];
+        f=0;
+       // cout<<temp[0]<<" ";
+        for(int i=1;i<temp.size();++i){
+            //cout<<temp[i]<<" ";
+            if(temp[i]>=temp[i-1])continue;
+            f=1;break;
+        }
+      //  cout<<endl;
+        
+        if(f)return false;
+        return true;
+    }
+};
