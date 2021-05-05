fe0e67f9df5d113f8d21092f8cdf27180c7d7e05
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-05 22:47:16 +0530

    Update B - Ordinary Numbers.cpp

--- Day56/B - Ordinary Numbers.cpp
+++ Day56/B - Ordinary Numbers.cpp
@@ -3,3 +3,32 @@ Platform :- Codeforces
 contest :- Codeforces Round #719 Div 3
 Problem :- Ordinary Number
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         long long n;
+         cin>>n;
+         
+         int c=0;
+         long long d=1;
+         for(long long i=1;i<=n;){
+             c++;
+             
+             string temp=to_string(i);
+             if(count(temp.begin(),temp.end(),'9')==temp.size()){
+                 d*=10;
+                 i=0;
+                 d+=1;
+             }
+             //cout<<c<<" "<<i<<endl;
+             i+=d;
+         }
+         cout<<c<<endl;
+    }
+}
