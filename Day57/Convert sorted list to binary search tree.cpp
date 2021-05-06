7e766b9fbe795ce6ec0fc2d9da4534d910504ca2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-06 22:19:18 +0530

    Update Convert sorted list to binary search tree.cpp

--- Day57/Convert sorted list to binary search tree.cpp
+++ Day57/Convert sorted list to binary search tree.cpp
@@ -2,4 +2,5 @@
 Platform :- Leetcode
 Problem:- Convert sorted list to binary search tree
 Event :- April Daily challenge
+Hint:-  Like merge sort we divide given array into subarrays and choose the middle element as next left or right node accordingly as array as sorted
 */
