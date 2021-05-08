002d84c2eb5bbbc8c5928266ca32f8e95cf3ec71
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-08 22:36:43 +0530

    Update B - 200th ABC-200.cpp

--- Day58/B - 200th ABC-200.cpp
+++ Day58/B - 200th ABC-200.cpp
@@ -3,3 +3,12 @@ Platform :- Atcoder
 Problem :- A - Century 
 Contest :- B - 200th ABC-200.cpp
 */
+n,k=map(int,input().split())
+for i in range(k):
+    if(n%200==0):
+        n/=200
+    else:
+        t=1000
+        temp=n*t
+        n=200+temp
+print(int(n))
