039897642854a35fad6d948a00d4eff3c506785f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-08 22:36:15 +0530

    Update A- Century.cpp

--- Day58/A- Century.cpp
+++ Day58/A- Century.cpp
@@ -3,3 +3,19 @@ Platform :- Atcoder
 Problem :- A - Century 
 Contest :- KYOCERA Programming Contest 2021（AtCoder Beginner Contest 200）
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    
+    if(n==1)cout<<1<<endl;
+    else{
+        
+        long long temp=n/100;
+        if(n%100)temp++;
+        cout<<temp<<endl;
+    }
+}
