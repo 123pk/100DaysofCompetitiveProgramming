c490c39cee6d820a7674723beb393be1f76dd7b8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-09 21:07:14 +0530

    Update A - Nastia and Nearly Good Numbers.cpp

--- Day59/A - Nastia and Nearly Good Numbers.cpp
+++ Day59/A - Nastia and Nearly Good Numbers.cpp
@@ -3,3 +3,26 @@ Platform :- Codeforces
 Contest :- Codeforces Round 720
 Problem :- A - Nastia and Nearly Good Numbers
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long A,B;
+        cin>>A>>B;
+        
+        long long y=A*B,z=y;
+        if(B==1)
+        {cout<<"NO"<<endl;
+        continue;}
+        
+      
+        cout<<"YES"<<endl;
+        cout<<(A)<<" "<<(y)<<" "<<(y+A)<<endl;
+        
+    }
+}
