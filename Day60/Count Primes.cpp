05aceee0ef62b87afc6913c1d3cc89eb93fed9dd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-10 22:41:25 +0530

    Update Count Primes.cpp

--- Day60/Count Primes.cpp
+++ Day60/Count Primes.cpp
@@ -1,4 +1,23 @@
 /*
 Platform :- Leetcode
 Problem :- Count Primes
-*/
+*/class Solution {
+public:
+    int countPrimes(int n) {
+        int ans=0;
+        int prime[5000001]={0};
+        for(int i=2;i*i<n;++i){
+            if(prime[i])continue;
+            
+            for(int j=i*i;j<=n;j+=i){
+                prime[j]+=1;
+            }
+        }
+        
+        for(int i=2;i<n;++i){
+            if(prime[i]==0)ans++;
+        }
+        
+        return ans;
+    }
+};
