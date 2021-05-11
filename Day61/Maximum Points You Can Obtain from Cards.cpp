fd5655ac4a3ebe54db9e5716651ce2cd3eaee4f1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-11 22:06:44 +0530

    Update Maximum Points You Can Obtain from Cards.cpp

--- Day61/Maximum Points You Can Obtain from Cards.cpp
+++ Day61/Maximum Points You Can Obtain from Cards.cpp
@@ -2,3 +2,33 @@
 Platform :- Leetcode
 Problem :- Maximum points You can Obtain from cards
 */
+class Solution {
+public:
+    int maxScore(vector<int>& A, int k) {
+        int n=A.size();
+        int pref[n],suff[n];
+        int tot=0;
+        for(int i=0;i<n;++i){
+            tot+=A[i];
+            pref[i]=tot;
+        }
+        
+        tot=0;
+        int j=0;
+        for(int i=n-1;i>=0;--i){
+            tot+=A[i];
+            suff[j]=tot;
+            j++;
+        }
+        
+        int ans=max(pref[k-1],suff[k-1]);
+        int temp=0;
+        for(int i=0;i<k-1;++i){
+            temp=pref[i]+suff[k-1-(i+1)];
+            ans=max(ans,temp);
+        }
+        
+        
+        return ans;
+    }
+};
