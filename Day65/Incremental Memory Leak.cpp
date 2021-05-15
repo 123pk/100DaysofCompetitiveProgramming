3bf6721eaa6c2a2a0b63604a1cf3783281190189
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-15 22:31:20 +0530

    Update Incremental Memory Leak.cpp

--- Day65/Incremental Memory Leak.cpp
+++ Day65/Incremental Memory Leak.cpp
@@ -2,3 +2,27 @@
 Platform :- Leetcode
 Problem :- Incremental Memory Leak
 */
+class Solution {
+public:
+    vector<int> memLeak(int a, int b) {
+        vector<int>ans;
+        
+        int temp=1;
+        int f=0;
+        while(1){
+            if(a>=b){
+                if(temp>a)break;
+                a-=temp;
+            }
+            else{
+                if(temp>b)break;
+                b-=temp;
+            }
+            temp++;
+            f++;
+        }
+        
+        ans={f+1,a,b};
+        return ans;
+    }
+};
