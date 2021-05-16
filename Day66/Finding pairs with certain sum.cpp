e08b61633e8e2a608d1e3c4ed678824664f3b420
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-16 23:08:26 +0530

    Create Finding pairs with certain sum.cpp

---
+++ Day66/Finding pairs with certain sum.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Leetcode
+Problem :- Finding pairs with certain sum
+Contest :-  Weekly contest 241
+Hint :- store data in two vectors. we need to find pair with ( x + y ) = k here x and y are elements from each vector we can simplify and check for occurence of (x) and (k-y)
+        using map
+*/
