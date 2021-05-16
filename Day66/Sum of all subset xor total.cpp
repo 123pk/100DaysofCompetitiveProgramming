b6cdf44db49b62aa9a97c14feb3d0f2b64251700
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-16 22:59:01 +0530

    Update Sum of all subset xor total.cpp

--- Day66/Sum of all subset xor total.cpp
+++ Day66/Sum of all subset xor total.cpp
@@ -3,3 +3,19 @@ Platform :- Leetcode
 Problem :- Sum of all subset xor total
 Contest :- Weekly contest 241
 */
+class Solution {
+public:
+  
+  
+    int subsetXORSum(vector<int>& arr) {
+        int bits = 0;
+        int n=arr.size();
+    // Finding bitwise OR of all elements
+     for (int i=0; i < n; ++i)
+         bits |= arr[i];
+ 
+      int ans = bits * pow(2, n-1);
+ 
+      return ans;
+    }
+};
