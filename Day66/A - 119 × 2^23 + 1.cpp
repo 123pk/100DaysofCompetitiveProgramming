2dec31666b1bc5e758300248497a71d76d95e646
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-16 23:12:28 +0530

    Create A - 119 × 2^23 + 1.cpp

---
+++ Day66/A - 119 × 2^23 + 1.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Atcoder
+Problem :- A - 119 × 2^23 + 1
+Contest :- Atcoder Regular contest 119
+Hint :- take a temp varible and run a loop  " while ( temp < = n ) " and increment temp to times 2 each time ( temp * = 2)
+        for each value of temp find the sum and store the minimum out of all
+
+*/
