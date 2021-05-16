d5641b7ba702bc038bc5ad8e0bebdd142b79afda
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-16 23:12:44 +0530

    Update A - 119 × 2^23 + 1.cpp

--- Day66/A - 119 × 2^23 + 1.cpp
+++ Day66/A - 119 × 2^23 + 1.cpp
@@ -6,3 +6,21 @@ Hint :- take a temp varible and run a loop  " while ( temp < = n ) " and increme
         for each value of temp find the sum and store the minimum out of all
 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    
+    long long temp=n,ans=1e18,x=1,d=0;
+    while(x<=n){
+        long long z=(temp/x);
+        z+=(temp%x);
+        z+=d;
+        ans=min(ans,z);
+        x*=2;
+        d++;
+    }
+    cout<<ans<<endl;
+}
