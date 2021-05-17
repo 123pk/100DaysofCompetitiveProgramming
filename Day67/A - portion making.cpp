8308f03d0b113b72593ac08529350035fc19e868
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-17 15:23:53 +0530

    Create A - portion making.cpp

---
+++ Day67/A - portion making.cpp
@@ -0,0 +1,26 @@
+/*
+Platform :- Codeforces
+Contest :- Educationl round  109
+Problem :- A - portion making
+*/
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    while(t--){
+        long long x;
+        cin>>x;
+        
+        long long temp=100-x;
+        temp=__gcd(x,temp);
+        
+        temp=(100/temp);
+        cout<<temp<<endl;
+    } 
+    
+}
