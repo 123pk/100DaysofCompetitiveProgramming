f86cf5fbe3c9c2111b3f05e9356d86d60c1161b6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-17 15:25:40 +0530

    Update B - Permutation sort.cpp

--- Day67/B - Permutation sort.cpp
+++ Day67/B - Permutation sort.cpp
@@ -3,3 +3,38 @@ Platform :- Codeforces
 Contest :- Educational round 109
 Problem :- B - Permutaion sort
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int f=0;
+        int A[n];
+         
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(A[i]!=(i+1)){
+                f=1;
+            }
+
+        }
+         
+        if(f==0)cout<<0<<endl;
+        else{
+              if(A[0]==1)cout<<1<<endl;
+              else if(A[n-1]==n)cout<<1<<endl;
+              else if(A[0]==n && A[n-1]==1)cout<<3<<endl;
+              else cout<<2<<endl;
+        }
+        
+        
+    } 
+    
+}
