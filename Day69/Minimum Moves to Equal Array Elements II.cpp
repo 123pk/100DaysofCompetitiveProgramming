9a17fc76ab9316acd256cf620e3cd94a0dc35c2c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-19 19:56:33 +0530

    Update Minimum Moves to Equal Array Elements II.cpp

--- Day69/Minimum Moves to Equal Array Elements II.cpp
+++ Day69/Minimum Moves to Equal Array Elements II.cpp
@@ -4,3 +4,28 @@ Problem :- Minimum Moves to Equal Array Elements II
 Approach :-  we will choose the middle element of sorted array nums if (nums.size() is odd)
              else we will choose the middle value of (nums.size()/2) & (nums.size()/2)-1
 */
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int ans=0;
+        
+        // we will choose the middle element of sorted array nums if (nums.size() is odd)
+        // else we will choose the middle value of (nums.size()/2) & (nums.size()/2)-1
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+         
+        int temp=0,part_val=0;
+        
+        if(n%2==0){
+            part_val=((nums[n/2]+nums[(n/2)-1]))/2;
+        }
+        else part_val=nums[(n/2)];
+        for(auto x:nums){
+            ans+=abs(x-part_val);
+        }
+        
+        return ans;
+    }
+};
