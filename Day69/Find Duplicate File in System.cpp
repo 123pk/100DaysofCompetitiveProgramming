43ed4ed9c032a6e3da4dad698b0274efeca9a293
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-19 09:15:43 +0530

    Create Find Duplicate File in System.cpp

---
+++ Day69/Find Duplicate File in System.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Find Duplicate File in System
+Approach :- we will try to find frequency of of contents of each  file using "map" in c++ , then if the frequency if greater than equal to 2 
+            we will add the address to our answer
+*/
