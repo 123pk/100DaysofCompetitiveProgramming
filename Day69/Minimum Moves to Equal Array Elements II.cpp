c33dd5f96132d3ed65bc90ee8083e482cac6d342
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-19 19:56:07 +0530

    Create Minimum Moves to Equal Array Elements II.cpp

---
+++ Day69/Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Minimum Moves to Equal Array Elements II
+Approach :-  we will choose the middle element of sorted array nums if (nums.size() is odd)
+             else we will choose the middle value of (nums.size()/2) & (nums.size()/2)-1
+*/
