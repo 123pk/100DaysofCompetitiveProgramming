b87fdfefce3085559e7ef350966eea1a7abf7e40
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-20 22:42:05 +0530

    Update A - And there were k .cpp

--- Day70/A - And there were k .cpp
+++ Day70/A - And there were k .cpp
@@ -3,3 +3,29 @@ Platform :- Codeforces
 Contest :- Codeforces round #721 
 Problem :- A - and there were k
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        if(n==1)cout<<0<<endl;
+        else{
+            string temp=bitset<64>(n).to_string();
+            int z=0;
+            reverse(temp.begin(),temp.end());
+            for(int i=0;i<64;++i){
+                if(temp[i]=='1')z=i;
+            }
+            
+            
+            long long ans=pow(2,z)-1;
+            cout<<ans<<endl;
+        }
+    }
+}
