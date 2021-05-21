95a46aefc73447969ff7485190958a8e7bb649fc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-21 20:04:34 +0530

    Update C - Sequence Pair weight.cpp

--- Day71/C - Sequence Pair weight.cpp
+++ Day71/C - Sequence Pair weight.cpp
@@ -3,3 +3,25 @@ Platform :- Codeforces
 Contest :- Codeforces Round 721
 Problem :- C - Sequence Pair weight 
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        map<long long,long long>P;
+        long long ans=0;
+        for(int i=0;i<n;++i){
+            long long x;
+            cin>>x;
+            
+            ans+=(P[x]*(n-i));
+            P[x]+=i+1;
+        }
+        cout<<ans<<endl;
+    }
+}
