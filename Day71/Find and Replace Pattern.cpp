cf5e663b7b278d77937fd25f7f804926677fcbd2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-21 22:11:04 +0530

    Update Find and Replace Pattern.cpp

--- Day71/Find and Replace Pattern.cpp
+++ Day71/Find and Replace Pattern.cpp
@@ -12,3 +12,42 @@ Approach :- i. filter string of same length in words .
 
 
                               
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string>ans;
+       
+        //storing index of occurence of that character in pattern string 
+        map<char,vector<int>>P;
+       
+        for(int i=0;i<pattern.size();++i){
+            P[pattern[i]].push_back(i+1);
+            //Q.insert(pattern[i]);
+        }
+        
+        
+        for(int i=0;i<words.size();++i){
+            
+            if(words[i].size() != pattern.size())continue;
+            
+            // storing index of occurence of that character in words[i] string 
+            map<char,vector<int>>R;
+            for(int j=0;j<words[i].size();++j){
+                R[words[i][j]].push_back(j+1);
+            }
+            
+            int f=0;
+             //comparing if the character at index j occur at same places in both the string or not
+            for(int j=0;j<pattern.size();++j){
+                if(P[pattern[j]]!=R[words[i][j]]){
+                    f=1;break;
+                }
+            }
+            
+             // if it satisfies the condition then adding it to our answer
+            if(f==0)ans.push_back(words[i]);
+        }
+        
+        return ans;
+    }
+};
