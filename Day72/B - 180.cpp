4d63fec30ed301c39adfed6eb87c5fde5fd0ef08
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-22 21:46:33 +0530

    Update B - 180.cpp

--- Day72/B - 180.cpp
+++ Day72/B - 180.cpp
@@ -3,3 +3,18 @@ Platform :- Atcoder
 Problem :- B - 180
 Contest :- Atcoder Beginner Contest 202
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      string s;
+      cin>>s;
+      
+      for(int i=0;i<s.size();++i){
+          if(s[i]=='6')s[i]='9';
+          else if(s[i]=='9')s[i]='6';
+      }
+      
+      for(int i=s.size()-1;i>=0;--i)cout<<s[i];
+      cout<<endl;
+}
