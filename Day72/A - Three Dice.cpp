f68f4c0c8d73730426d7ae65fb3bd8a45c64e00f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-22 21:46:53 +0530

    Update A - Three Dice.cpp

--- Day72/A - Three Dice.cpp
+++ Day72/A - Three Dice.cpp
@@ -3,3 +3,12 @@ Platform :- Atcoder
 Problem :- A - Three Dice
 Contest :- Atcoder Beginner Contest 202
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int a,b,c;
+      cin>>a>>b>>c;
+      
+      cout<<((7-a)+(7-b)+(7-c))<<endl;
+}
