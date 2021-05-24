2b2f351302a9f7aae215d206a2ec341e707a1bdd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-24 22:59:52 +0530

    Update A - Eshang Love Big arrays.cpp

--- Day74/A - Eshang Love Big arrays.cpp
+++ Day74/A - Eshang Love Big arrays.cpp
@@ -3,3 +3,27 @@ Platform :- Codeforces
 contest :- Codeforces Round 722
 Problem :- A - Eshang Love Big Arrays
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        map<long long,int>P;
+        long long A[n];
+        long long mn=1e18;
+        int mx=0;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            P[A[i]]++;
+            mn=min(mn,A[i]);
+        }
+        mx=P[mn];
+        cout<<(n-mx)<<endl;
+    }
+}
