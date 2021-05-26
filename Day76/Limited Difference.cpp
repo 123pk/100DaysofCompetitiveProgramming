fefd0901cff7afa7e64955ca5aa09e3f32d3827e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-26 22:44:51 +0530

    Create Limited Difference.cpp

---
+++ Day76/Limited Difference.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Topcoder
+Contest :- Limited Difference
+Contest :- SRM 806
+Hint :- case 1 . if V = 1 , then answer is [1,2...,n]
+            case 2. for a particular V the value of differences possible are from [1 to V] create a array in descending ordr from V to 1 
+                    for each of the value greedily chose element to put in array
+*/
