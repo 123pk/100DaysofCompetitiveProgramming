8a54e6622d6ebcb7d48201245d78b4c065f3af2a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-26 22:28:50 +0530

    Create SudokoTypo.cpp

---
+++ Day76/SudokoTypo.cpp
@@ -0,0 +1,6 @@
+/*
+Platform : - Topcoder
+Problem :- Sudoko Typo
+Contest :- SRM 806
+Approach :- check for repeating values in 3*3 sub matrix check for value from 1 to 9 . elements thta is missing is our answer
+*/
