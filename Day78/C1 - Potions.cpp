b93335cab06174db324d519177135f46f46c89a6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-28 23:27:44 +0530

    Create C1 - Potions.cpp

---
+++ Day78/C1 - Potions.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 723 Div 2
+Problem :- C1 Potions
+Approch :- I was not able to figure out for solution during contests but it was an good proble
+           Approach is to take a variable tot and add value A[i] for 0<=i<n and increment our "count"
+            i . if A[i]<0 then add it to min priority queue
+            ii. if tot<0 we subtract the smallest value of priority queue from tot and decrement our "count"
+*/
