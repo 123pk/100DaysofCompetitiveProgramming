236532bca7ef6e22b3872de1ab9ae21684768af9
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-28 23:21:36 +0530

    Create B - I hate 1111.cpp

---
+++ Day78/B - I hate 1111.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 723 Div 2
+Problem :- B-I hate 1111
+Approach :- There is easy solution of this one liner you can check out in editorial 
+           My approach was to check all combination of values using recursion which add up to n as no of values are not more than 8 
+           so no issue of TLE
+*/
