003a9c30b4753f838920719d8dd4c8c9bc9d2948
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-28 19:18:19 +0530

    Create Maximum Product of Word Lengths.cpp

---
+++ Day78/Maximum Product of Word Lengths.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Leetcode
+Problem :- Maximum Product of Word Lengths
+Hint :- there are 26 alphabet in english so we can replace each string with "vector" of 26 size which will store details about occurence of character.
+        Compare each pair of index with their vectors 
+        Run time is O(N^2* 26).
+*/
+
