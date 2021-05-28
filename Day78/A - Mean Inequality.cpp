e163a6c29ff2606228b04e82991bf65336a3a2a6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-28 23:14:49 +0530

    Create A - Mean Inequality.cpp

---
+++ Day78/A - Mean Inequality.cpp
@@ -0,0 +1,5 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 703
+Approach :- sort the array in increasing order and then  " swap( A[i], A[i+1]) " for odd values of i ( 0 based indexing ) and (i+1< n)
+*/
