dcbf4c3802ef74863b61786da30714cadfd80a1b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-28 23:29:07 +0530

    Update C1 - Potions.cpp

--- Day78/C1 - Potions.cpp
+++ Day78/C1 - Potions.cpp
@@ -7,3 +7,30 @@ Approch :- I was not able to figure out for solution during contests but it was 
             i . if A[i]<0 then add it to min priority queue
             ii. if tot<0 we subtract the smallest value of priority queue from tot and decrement our "count"
 */
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    
+    long long A[n];
+    for(int i=0;i<n;++i)cin>>A[i];
+    
+    int ans=0;
+    long long tot=0;
+    priority_queue<long long,vector<long long>,greater<long long>>P;
+    for(int i=0;i<n;++i){
+        if(A[i]<0)P.push(A[i]);
+        
+        tot+=A[i];
+        ans++;
+        
+        if(tot<0){
+            tot-=P.top();
+            P.pop();
+            ans--;
+        }
+    }
+    cout<<ans<<endl;
+}
