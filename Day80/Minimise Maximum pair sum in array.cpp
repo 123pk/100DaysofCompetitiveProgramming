98a03b76392e883b86f2c576632afc5ae436d55a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-30 09:49:59 +0530

    Update Minimise Maximum pair sum in array.cpp

--- Day80/Minimise Maximum pair sum in array.cpp
+++ Day80/Minimise Maximum pair sum in array.cpp
@@ -3,3 +3,15 @@ Platform :- Leetcode
 Problem :- Minimise Maximum pair sum in array
 Contest :- Bi weekly contest 53
 */
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()/2;i++){
+            ans=max(ans,nums[i]+nums[nums.size()-1-i]);
+        }
+        
+        return ans;
+    }
+};
