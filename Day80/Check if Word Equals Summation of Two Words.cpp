859efba65f49c58644be2d72f19d6c622de0eece
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-30 09:59:35 +0530

    Update Check if Word Equals Summation of Two Words.cpp

--- Day80/Check if Word Equals Summation of Two Words.cpp
+++ Day80/Check if Word Equals Summation of Two Words.cpp
@@ -3,3 +3,25 @@ Platform :- Leetcode
 Problem :- Check if Word Equals Summation of Two Words
 Contest :- Weekly contest 243
 */
+class Solution {
+public:
+    bool isSumEqual(string A, string B, string C) {
+        long long temp1=0,temp2=0,temp3=0;
+        
+        for(auto x:A){
+            temp1*=10;
+            temp1+=(x-'a');
+        }
+        for(auto x:B){
+            temp2*=10;
+            temp2+=(x-'a');
+        }
+        for(auto x:C){
+            temp3*=10;
+            temp3+=(x-'a');
+        }
+        
+        temp1+=temp2;
+        return temp1==temp3;
+    }
+};
