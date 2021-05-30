dc8401913843e2466cc49f405a48199c6051c22a
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-30 09:47:17 +0530

    Update substring of size three with distinct characters.cpp

--- Day79/substring of size three with distinct characters.cpp
+++ Day79/substring of size three with distinct characters.cpp
@@ -3,3 +3,20 @@ Platform :- Codeforces
 Contest :- Bi weekly contest 53
 Problem :- Substring of size three with Distinct characters
 */
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        set<char>P;
+        int ans=0;
+        if(s.length()<3)return ans;
+        int f=0;
+        for(int i=2;i<s.size();++i){
+            set<char>P;
+            P.insert(s[i]);
+            P.insert(s[i-1]);
+            P.insert(s[i-2]);
+            if(P.size()==3)ans++;
+        }
+        return ans;
+    }
+};
