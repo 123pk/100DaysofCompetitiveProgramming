f601f5db258869acc0f73ff4c4ba5af95d938c38
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-05-31 23:03:08 +0530

    Update TANDJ1.cpp

--- Day81/TANDJ1.cpp
+++ Day81/TANDJ1.cpp
@@ -2,4 +2,26 @@
 Platform :- Codechef
 Problem :- TANDJ1
 Contest :- Codechef Lunchtime
+Hint :- if we can reach our destination in 'x' steps and 'x' < k then we will move around and will only be able to come back if (k-x) is even
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b,c,d,k;
+        cin>>a>>b>>c>>d>>k;
+        
+        long long temp=abs(a-c)+abs(b-d);
+        if(k<temp)cout<<"NO"<<endl;
+        else{
+            long long z=k-temp;
+            if(z%2)cout<<"NO"<<endl;
+            else cout<<"YES"<<endl;
+        }
+        
+    }
+}
