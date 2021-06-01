6d87dd4fe0640033953ed81524f78bd95162daf0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-01 20:46:44 +0530

    Create A - Winner.cpp

---
+++ Day82/A - Winner.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Beta Round 2
+Problem :- A - Winner
+Hint :- find the strings with max value then , out of those string find the string which gets to max value first
+*/
