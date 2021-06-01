1449b41a82294b9f850a85577828ccfd9bbe8db3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-01 21:12:28 +0530

    Create TWINGFT.cpp

---
+++ Day82/TWINGFT.cpp
@@ -0,0 +1,8 @@
+/*
+Platform :- Codechef
+Problem :- TWINGFT
+Contest :- Codechef lunchtime
+Approach :- take in mind thaat in last move one playe two values , we need to maximise our score so we will sort the array in decreasing order 
+            then in alternate turn we will choose the max value left except for last turn . 
+            The one with maximum value is our solution
+*/
