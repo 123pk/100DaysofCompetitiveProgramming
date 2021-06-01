f078a7b2826c388c3ef818307463f950adaa6780
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-01 23:33:42 +0530

    Create C -Good string.cpp

---
+++ Day82/C -Good string.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Educational round 92
+Problem :- C - Good string
+Approach :- since there are only 10 digits ( 0 to 9) are present we can bruteforce all the combination and we will find the max even length if 
+            we have if we have different digits , otherwise we have maximum count of a digit.
+*/
