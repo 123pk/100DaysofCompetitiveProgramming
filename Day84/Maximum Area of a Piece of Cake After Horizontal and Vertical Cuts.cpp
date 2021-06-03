2241faada9104b3fa740d0cc40b3e24b423f3284
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-03 23:04:55 +0530

    Create Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp

---
+++ Day84/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+Approach :- (max_area = max_height * max_wdth )  sort both arrays and find the maximum difference between two cosecutive numbers that will give us value
+            of max_height and max_width . Take care of starting and ending value ( 0,r) and (0,c) where r and c are upper limit of height and width
+*/
