4d038b9adbb943ff9b9022de9f80b0fdee7c7fab
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-04 00:47:34 +0530

    Update D - Python triples.cpp

--- Day85/D - Python triples.cpp
+++ Day85/D - Python triples.cpp
@@ -10,3 +10,31 @@ Approach :- we need to simplify the given formulae and pythagoras theorem
            * we can find value of [ 'b' = ( a^2 -1 )/2 ]  and 'c' = [ a^2 - b ] .
            * we will run an while loop until our value of 'c' is <= n  and increment our answer count
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        if(n<5){cout<<"0"<<endl;continue;}
+        long long ans=0;
+        long long a=3,b=0,c=0;
+        while(1){
+        
+            long long temp=a*a;
+            b=((temp)-1)/2;
+            c=(temp)-b;
+            if(c<=n)ans++;
+            else break;
+           // cout<<a<<" "<<b<<" "<<c<<" "<<ans<<endl;
+            a+=2;
+        }
+        cout<<ans<<endl;
+        
+    }
+}
