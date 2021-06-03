f8fbd75678ee9b1941d1a1f6ad73105fc8efcfe6
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-03 22:57:24 +0530

    Update A - Two substring.cpp

--- Day84/A - Two substring.cpp
+++ Day84/A - Two substring.cpp
@@ -2,4 +2,5 @@
 Platform :- Codeforces
 Problem :- A - Two Substring
 Contest :- Codeforces Round 306 Div 2
+Approach :- We will check if we have ("AB" and "BA") or ("BA" and "AB") as disjoint substring or not. 
 */
