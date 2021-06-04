0d6df4b7a94ad508dd546aa5e27fed8f0b9d480f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-04 22:45:20 +0530

    Update D2 - Sage Birthdays.cpp

--- Day85/D2 - Sage Birthdays.cpp
+++ Day85/D2 - Sage Birthdays.cpp
@@ -3,3 +3,42 @@ Platform :- Codeforces
 problem :- D2 - Sage Birthday
 Contest :- Codeforces Round 671
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    long long A[n];
+    for(int i=0;i<n;++i)cin>>A[i];
+    sort(A,A+n);
+    
+    if(n<=2){
+        cout<<0<<endl;
+        for(auto x:A)cout<<x<<" ";
+        exit(0);
+    }
+    
+    int m=n/2;
+    int lim=m;
+    int i=0;
+    vector<long long>B;
+    while(1){
+        if(m<n)B.push_back(A[m]);
+        m++;
+        if(i<lim)B.push_back(A[i]);
+        else break;
+        i++;
+        
+    }
+    
+    int count=0;
+    for(int i=1;i<n-1;++i){
+        if(B[i]<B[i-1] && B[i]<B[i+1])count++;
+    }
+    
+    cout<<count<<endl;
+    for(auto x:B)cout<<x<<" ";
+    cout<<endl;
+}
