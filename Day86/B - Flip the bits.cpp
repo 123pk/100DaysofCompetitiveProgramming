8595d479fdaad4faa5d56c64e4e4428e7243b716
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-05 22:38:11 +0530

    Create B - Flip the bits.cpp

---
+++ Day86/B - Flip the bits.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 712
+Problem :- B - Flip the bits
+Hint:- the points( index) where strings are not equal should also be the points where ( no_of_ones = no_of_zeros )
+*/
