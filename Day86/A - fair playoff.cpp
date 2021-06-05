8e4f94b2db1209751fc1f8287f84d4d600c27a81
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-05 22:11:57 +0530

    Update A - fair playoff.cpp

--- Day86/A - fair playoff.cpp
+++ Day86/A - fair playoff.cpp
@@ -3,3 +3,30 @@ Platform :- Codeforces
 Contest : - Educational round 110
 Problem :- A - fair playoff
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(pair<long long ,int>P,pair<long long,int>Q){
+    
+    return P.first>Q.first;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b,c,d;
+        cin>>a>>b>>c>>d;
+        
+        long long temp=max(a,b);
+        long long temp2=max(c,d);
+        
+        long long A[4]={a,b,c,d};
+        sort(A,A+4);
+        
+        if((A[3]==temp && A[2]==temp2) ||(A[3]==temp2 && A[2]==temp))cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        
+    }
+}
