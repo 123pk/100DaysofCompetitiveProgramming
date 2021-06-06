eafc091f37bb2ebc0e38820f991b40ba1768d980
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-06 23:01:37 +0530

    Update B - Nuts.cpp

--- Day87/B - Nuts.cpp
+++ Day87/B - Nuts.cpp
@@ -3,3 +3,18 @@ Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 204
 Problem :- B - Nuts
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    long long ans=0;
+    long long A[n];
+    for(int i=0;i<n;++i){
+        cin>>A[i];
+        if(A[i]>10)ans+=(A[i]-10);
+    }
+    cout<<ans<<endl;
+}
