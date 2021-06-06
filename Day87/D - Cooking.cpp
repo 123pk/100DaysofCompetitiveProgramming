134a93d1e904ef0a3da8713f06e5f9609b7143e0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-06 22:59:25 +0530

    Create D - Cooking.cpp

---
+++ Day87/D - Cooking.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Atcoder
+Contest :- Atcoder Beginner Contest 204
+Problem :- D - Cooking
+Approach :- This problem is slight variation of [ https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/]
+            Instead of minimum difference we need to find the ( max time) which is ( min difference + min value )
+*/
