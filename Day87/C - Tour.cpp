c325e538432aca875de2ae4cf4a596e1acf88c66
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-06 23:00:58 +0530

    Update C - Tour.cpp

--- Day87/C - Tour.cpp
+++ Day87/C - Tour.cpp
@@ -2,4 +2,5 @@
 Platform :- Atcoder
 Contest :- Atcoder Beginner Contest 204
 Problem :- C - Tour
+Approach :- do BFS for all values from 1 to n , and find all the distinct pairs you can visit ,ans is  ( n+( total_distinct_pairs) )
 */
