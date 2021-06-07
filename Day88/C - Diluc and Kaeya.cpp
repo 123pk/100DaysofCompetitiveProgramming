5b1ef964318abddf35ad6be390f067c45f0c7471
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-07 14:52:19 +0530

    Update C - Diluc and Kaeya.cpp

--- Day88/C - Diluc and Kaeya.cpp
+++ Day88/C - Diluc and Kaeya.cpp
@@ -3,3 +3,29 @@ Platform :- Codeforces
 Contest :- Codeforces Round 724
 Problem :- C - Diluc and Kaeya
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+          int n;
+          cin>>n;
+          
+          string s;
+          cin>>s;
+          
+          map<pair<int,int>,int>P;
+          int d=0,k=0;
+          for(auto x:s){
+              if(x=='D')d++;
+              else k++;
+              
+              int temp=__gcd(k,d);
+              cout<<(++P[{d/temp,k/temp}])<<" ";
+          }
+          cout<<endl;
+    }
+}
