c7a5a997299a8376cf8b8442a87e7a6479366fe8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-08 22:27:30 +0530

    Create B - Box fitting.cpp

---
+++ Day89/B - Box fitting.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- CodeCraft-21 and Codeforces Round #711 (Div. 2)
+Problem :- B - Box fitting
+Approach :- We start with the biggest box that can be fit in left over space , if there are no such boxes then we increment the height so left over space become
+            max_possible again and we repeat this until last box is placed
+*/
