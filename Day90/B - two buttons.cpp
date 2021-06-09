3c943cd3d9ca89b971063fd3a45ea1f58e4f5ce1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-09 23:39:25 +0530

    Create B - two buttons.cpp

---
+++ Day90/B - two buttons.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces Round 295
+Problem :- two bottoms
+Approach :-  if m<n then we have only one option is to substract 1 from it and it will take (n-m) steps ,
+             else if "m" is odd it will take 2 steps , we will multiply by 2 and then substract 1, if it is even it will take one step , we will do this until we get m<n
+*/
