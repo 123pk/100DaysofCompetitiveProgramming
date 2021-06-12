e18f5c5d0bbfa285dfc15b5b0637818015c4fbae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-12 22:02:44 +0530

    Create Find the Student that Will Replace the Chalk.cpp

---
+++ Day93/Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,7 @@
+/* 
+Platform :- Leetcode 
+Problem :- Find the Student that Will Replace the Chalk 
+Contest :- Bi weekly contest 54
+Approach :- find total sum of time need for all students , then find the remainder ( k % tot) , then start traversing from left the index at which the  k< chalk[i]
+            reuturn that index
+*/
