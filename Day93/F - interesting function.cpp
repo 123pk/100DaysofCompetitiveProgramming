d38a1ce65a98ad3cf2c8eac3555eb1deac87ec3d
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-12 17:16:08 +0530

    Update F - interesting function.cpp

--- Day93/F - interesting function.cpp
+++ Day93/F - interesting function.cpp
@@ -11,3 +11,34 @@ Approach :- You need to analyse the problem on certain test case , You will find
             * and 100 is divisible by 100 so one more digit is changed , hence total count = '3' which is our answer ,
             similarly we will check for all 10,100,1000,10000....till 100000000 .
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long a,b;
+        cin>>a>>b;
+        
+        long long ans=(b-a);
+        long long temp=a;
+        long long c1=0;
+        while(1){
+            c1+=(temp/10);
+            temp/=10;
+            if(temp==0)break;
+        }
+        
+        temp=b;
+        long long c2=0;
+        while(1){
+            c2+=(temp/10);
+            temp/=10;
+            if(temp==0)break;
+        }
+        ans+=(c2-c1);
+        cout<<ans<<endl;
+    }
+}
