33941ba2bcc39e7bfbae2c8cd0d569437a3e2854
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 11:56:16 +0530

    Update D - kth excluded.cpp

--- Day94/D - kth excluded.cpp
+++ Day94/D - kth excluded.cpp
@@ -1,1 +1,7 @@
-
+/*
+Platform :- Atcoder
+Problem :- D - kth excluded
+Contest :- Atcoder Beginner contest 205
+Approach :- binary in the array till the number of missing value is less than required 'k' for each query .
+            Time complexity of program :- O( q*log(n))
+*/
