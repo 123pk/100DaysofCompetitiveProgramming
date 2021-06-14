56870a38605620f0c80f88a055f01a481adfd0ae
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 22:31:06 +0530

    Create A - Colour the flag.cpp

---
+++ Day95/A - Colour the flag.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Contest :- Codeforces LATOKEN Round 1 (Div. 1 + Div. 2)
+Problem :- A - Colour The flag
+Approach :- Try to color flag in WRWRW.. pattern if at any point it is not possible try to form RWRWR.. pattern if both of the pattern is not possible print ("NO") 
+            else print("YES") and pattern that satisfied condition.
+*/
