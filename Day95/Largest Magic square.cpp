42182a643cb0b2f58c68f9d79cb1498c20fbe9ad
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 22:24:46 +0530

    Create Largest Magic square.cpp

---
+++ Day95/Largest Magic square.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem :- Largest Magic Square
+Approach :- Find prefix sum column wise and row wise. Now we will try to find all squre of size 'k' from 2 to 50 , as matrix length is atmost 50,\
+            If we find magic square we will store the max length out of all possible magic square .
+*/
