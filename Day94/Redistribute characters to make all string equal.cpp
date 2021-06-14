9351dc760224f66fa6fc6c9cfc3163712ecb8e61
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 11:53:55 +0530

    Update Redistribute characters to make all string equal.cpp

--- Day94/Redistribute characters to make all string equal.cpp
+++ Day94/Redistribute characters to make all string equal.cpp
@@ -1,1 +1,23 @@
+/*
+Platform :- Leetcode
+Problem :- Redistribute characters to make all string equal
+Contest :- Weekly contest 245
+Approach :-  if there are 'm' disctinct chanracters then each character must ocuur multiple of 'm' times
+*/
 
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        
+        map<char,int>P;
+        for(auto x:words){
+            for(auto y:x)P[y]++;
+        }
+        
+        int start=0;int m=words.size();
+        for(auto x:P){
+             if(x.second%m)return false;
+        }
+        return true;
+    }
+};
