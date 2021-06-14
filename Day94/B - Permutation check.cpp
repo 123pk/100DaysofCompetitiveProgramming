fd71c66854aa73e7c977f658cd8a9d761c3a2bcc
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 12:03:13 +0530

    Update B - Permutation check.cpp

--- Day94/B - Permutation check.cpp
+++ Day94/B - Permutation check.cpp
@@ -4,3 +4,26 @@ Problem :- B - Permutation check
 Contest :- Atcoder Contest 205
 Approach :- sort array and if for all i from ( 0 to n-1) A[i] = i+1 then print "Yes" else "No"
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    long long A[n];
+    for(int i=0;i<n;++i){
+        cin>>A[i];
+    }
+    
+    sort(A,A+n);
+    int f=0;
+    for(int i=0;i<n;++i){
+        if(A[i]!=(i+1)){
+            f=1;break;
+        }
+    }
+    if(f)cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
+}
