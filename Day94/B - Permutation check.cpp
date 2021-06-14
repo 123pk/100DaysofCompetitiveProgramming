7b0eb79209285ac579ef839545ee49d93ce10828
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 12:03:01 +0530

    Update B - Permutation check.cpp

--- Day94/B - Permutation check.cpp
+++ Day94/B - Permutation check.cpp
@@ -1,1 +1,6 @@
-
+/*
+Platform :- Atcoder
+Problem :- B - Permutation check
+Contest :- Atcoder Contest 205
+Approach :- sort array and if for all i from ( 0 to n-1) A[i] = i+1 then print "Yes" else "No"
+*/
