0572f68d135589cade6cb56433f55ccac9f1a4ac
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 22:38:08 +0530

    Create C - Little Alawn's Puzzle.cpp

---
+++ Day95/C - Little Alawn's Puzzle.cpp
@@ -0,0 +1,7 @@
+  
+/*
+Platform :- Codeforces 
+Contest :- Codeforces LATOKEN Round 1 (Div. 1 + Div. 2)
+Problem :- C - Little Alawn's Puzzle
+Approach :- 2^No of connected componenets in array 
+*/
