eef604c6a309ddee8323ccf616f98aacc550a50b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-14 12:00:55 +0530

    Update C - Pow.cpp

--- Day94/C - Pow.cpp
+++ Day94/C - Pow.cpp
@@ -1,1 +1,10 @@
-
+/*
+Platform :- Atcoder
+Problem :- C - pow
+Contest :- Atcoder Beginner contest 205
+Approach :- there are three cases 
+            Case 1 :- both 'a' & 'b' are +ve in this case which ever element is greater will have greater 'c'th power  value
+            Case 2 :- either one is negative here if 'c' is even then element with highest absolute value will have greater cth power , else if odd power then 
+                      value which is positive has greater value
+            Case 3 :- both negative same as case 1 
+*/
