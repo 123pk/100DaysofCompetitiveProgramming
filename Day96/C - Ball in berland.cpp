b56d31bb3dfcaddb237f1adb69cfc4614ab17064
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-15 21:22:37 +0530

    Update C - Ball in berland.cpp

--- Day96/C - Ball in berland.cpp
+++ Day96/C - Ball in berland.cpp
@@ -8,3 +8,37 @@ Approach :- Calcuate the occurnece of each 'boy' in dance pair and each 'girl' i
             substract '1' from total occurences of current boy and current girl in valid pairs -->(x-1) & (y-1) respectively .
             So we can map them with ( (k-1)- ((x-1)+(y-1)) ) pairs respectively for each valid pairs.
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,k;
+        cin>>a>>b>>k;
+        
+        int A[k],B[k];
+        int c[a]={0},d[b]={0};
+        
+        for(int i=0;i<k;++i){
+            
+            cin>>A[i];
+            A[i]--;
+            c[A[i]]++;
+        }
+        
+        for(int i=0;i<k;++i){
+            cin>>B[i];
+            B[i]--;
+            d[B[i]]++;
+        }
+        
+        long long ans=0;
+        for(int i=0;i<k;++i){
+            ans+=((k+1)-(c[A[i]]+d[B[i]]));
+        }
+        ans/=2;
+        cout<<ans<<endl;
+    }
+}
