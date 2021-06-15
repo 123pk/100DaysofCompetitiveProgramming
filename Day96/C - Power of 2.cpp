c92020f6ff122e3ba32fb6314c8a0d6b0b1ad322
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-15 21:28:12 +0530

    Update C - Power of 2.cpp

--- Day96/C - Power of 2.cpp
+++ Day96/C - Power of 2.cpp
@@ -1,6 +1,6 @@
 /*
 Platform :- Codeforces 
-Contest :- 
+Contest :- Codeforces Round 529 Div 3
 Problem :- C - Power of 2
 Approach :- First find bits representation of given number , if no of '1's in bits representation are > k then  it is never possible .
             else we push (2,i) where ith bit is '1' to our priority queue , then we start dividing the max element by 2 and inccrease value of 'count'as soon as it becomes
