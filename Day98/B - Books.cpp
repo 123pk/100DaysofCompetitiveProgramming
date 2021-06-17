011aef0f5503892e28714d05749cb0b41474a70b
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-17 22:30:31 +0530

    Create B - Books.cpp

---
+++ Day98/B - Books.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Problem :- B - Books
+Contest :- Codeforces Round 171 Div 2
+Approach :- Used sliding window technique to slide from left to right and find the sum , as the sum becomes greater than 'k' then we start removing element from start of window
+            until the sum is <='k' and maintain and change count accordiny
+ */
