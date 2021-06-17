68d6044e05c29ff52c39e72f5d45f4d0df69a190
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-17 22:31:56 +0530

    Update B - Books.cpp

--- Day98/B - Books.cpp
+++ Day98/B - Books.cpp
@@ -5,3 +5,45 @@ Contest :- Codeforces Round 171 Div 2
 Approach :- Used sliding window technique to slide from left to right and find the sum , as the sum becomes greater than 'k' then we start removing element from start of window
             until the sum is <='k' and maintain and change count accordiny
  */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    long long t;
+    cin>>n>>t;
+    
+    int ans=0;
+    long long A[n];
+    int start=0;
+    for(int i=0;i<n;++i){
+        cin>>A[i];
+    }
+    
+    int d=0;
+    long long tot=0;
+    //start stores the start of our current subarray ( left side of our window)
+    for(int i=0;i<n;++i){
+        long long temp=A[i]+tot;
+        if(temp>t){
+            ans=max(ans,d);
+            d++;
+         
+            while(temp>t  && start<=i){
+                 temp-=A[start];
+                 
+                 d--;
+                 start++;
+                 if(temp<=t)break;
+            }
+            tot=temp;
+        }
+        else{
+            d++;
+            tot+=A[i];
+            if(i==n-1)ans=max(ans,d);
+        }
+    }
+    
+    cout<<ans<<endl;
+}
