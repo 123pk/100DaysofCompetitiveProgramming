aae35a70da0c82ae20c98668669c4038a731ea22
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-17 22:36:50 +0530

    Create D - Divide by three, multiply by two.cpp

---
+++ Day98/D - Divide by three, multiply by two.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round #479 (Div. 3)
+Problem :- Divide by three, multiply by two
+Approach :- we store frequency of each element in array , we run a loop on array and for each element we try to find all the values which are preset in array
+            if number of values is equal tto size of array then it means we got our answer and we break out of loop and print array
+*/
