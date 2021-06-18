8d2a68e0a6cbeb2604262f43b84408a859e36b11
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-19 00:09:17 +0530

    Create E1 - Erase and extend ( easy version).cpp

---
+++ Day100/E1 - Erase and extend ( easy version).cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 726 # Div 2
+Problem :- E1 - Erase and extend( easy version)
+Approach :- find a substring from start of main string such that each character is less than  or equal to first character of array
+            find all the string possible by taking prefix of substring we got from above
+*/
