0aa9ac97ef6eb6304a5204c33ee6030ab139e537
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-18 23:02:56 +0530

    Create C - Challenge Clifs.cpp

---
+++ Day99/C - Challenge Clifs.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces 
+Problem :- C - Challenege Clifs
+Contest :- Codeforces Round 726 #Div 2 
+Approach :- sort the array in ascending order , if n==2 print the sorted array else find the smallest value with minimum difference , then partition array into two parts [0:i] and [i+1,n]
+           print elements [i+1,n] first then [0:i]
+*/
