e43e5cfbf1e97d379c3ea87bae75389869280449
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-18 23:18:56 +0530

    Update B - Bad Boy.cpp

--- Day99/B - Bad Boy.cpp
+++ Day99/B - Bad Boy.cpp
@@ -4,3 +4,18 @@ Contest :- Codeforces Contest 726 Div 2
 Problem :- B - Bad Boy
 Approach :- any two diagonally opposite corner points in grid 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+         long long n,m,x,y;
+         cin>>n>>m>>x>>y;
+         
+        cout<<"1 1 "<<n<<" "<<m<<endl;
+    }
+}
