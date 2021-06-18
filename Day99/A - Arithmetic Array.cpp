caceb4cc2a79b72a8378b385201bfcd1765ada1c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-18 22:35:46 +0530

    Create A - Arithmetic Array.cpp

---
+++ Day99/A - Arithmetic Array.cpp
@@ -0,0 +1,7 @@
+/*
+Platform :- Codeforces
+Contest :- Codeforces Round 726 #Div 2
+Problem :- A - Arithmetic Array
+Approach :- find the sum of values of array if (sum<n) ans=1 , as we can add ((n-(sum))+1) value to array which will give us mean =1 , else 
+            we need to add ( sum - n) 0's to array to make mean =1 so ans in this case is (sum-n)
+*/
