ef0d60f667cc3046c7ae995500d3c3c023c650c2
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-19 22:43:04 +0530

    Update A - maxi buying.cpp

--- Day100/A - maxi buying.cpp
+++ Day100/A - maxi buying.cpp
@@ -3,3 +3,16 @@ Platform :- Atcoder
 Problem :- A - maxi buying 
 Contest :- Atcoder Beginner Contest 206
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double x;
+    cin>>x;
+    
+    long long z=(x*1.08);
+    if(z==206)cout<<"so-so"<<endl;
+    else if(z>206)cout<<":("<<endl;
+    else cout<<"Yay!"<<endl;
+    
+}
