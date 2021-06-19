2e570cef3470c28cdf3dd724c2a0ad4e4544dac1
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-06-19 22:39:48 +0530

    Update B - savings.cpp

--- Day100/B - savings.cpp
+++ Day100/B - savings.cpp
@@ -3,3 +3,31 @@ Platform :- Atcoder
 Problem :- B - savings
 Contest :- Atcoder Beginner Contest 206
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int n;
+     cin>>n;
+     
+     long long A[n];
+     map<long long,int>P;
+     vector<long long>temp;
+     for(int i=0;i<n;++i){
+         cin>>A[i];
+         P[A[i]]++;
+         if(P[A[i]]==1)temp.push_back(A[i]);
+     }
+     
+     sort(temp.begin(),temp.end());
+     long long ans=0;
+     int sz=n;
+     for(int i=temp.size()-1;i>=0;--i){
+         long long z=sz-P[temp[i]];
+         z*=P[temp[i]];
+         ans+=z;
+         sz-=P[temp[i]];
+     }
+     
+     cout<<ans<<endl;
+}
